210bd2e4263ae62fc92e14d94f73ac04743a0d64
AMIT VERMA <amitverma.dce@gmail.com>
2023-05-12 01:52:13 +0530

    Listing Data in Views and Templates

--- config/config/views.py
+++ config/config/views.py
@@ -6,11 +6,12 @@ import random
 def home_view(request):
     random_int = random.randint(1, 3)
     article_obj = Article.objects.get(id=random_int)
+    article_queryset = Article.objects.all()
     context = {
+        "object_list": article_queryset,
         "title": article_obj.title,
         "content": article_obj.content,
         "id": article_obj.id
-
     }
 
     HTML_STRING = render_to_string("home_view.html", context=context)   
