a0bc29c4381613075ca024e3c0e9bef84a1fcdbc
AMIT VERMA <amitverma.dce@gmail.com>
2023-05-12 01:25:54 +0530

    Rendering Templates
    Using render_to_string method

---
+++ config/config/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from articles.models import Article
+import random
+
+def home_view(request):
+    random_int = random.randint(1, 3)
+    article_obj = Article.objects.get(id=random_int)
+    context = {
+        "title": article_obj.title,
+        "content": article_obj.content,
+        "id": article_obj.id
+
+    }
+
+    HTML_STRING = render_to_string("home_view.html", context=context)   
+    return HttpResponse(HTML_STRING)
