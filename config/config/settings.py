edf67bab462bec27c2c11187bab860d8f2457b5d
AMIT VERMA <amitverma.dce@gmail.com>
2023-05-16 20:02:46 +0530

    Environment Variables & Dotenv edited

--- config/config/settings.py
+++ config/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rcezfzv793g@-jwyqk)etw$l6lsdif@&z^473jkn7gh2+w%i-0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))=='1'
