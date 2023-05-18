ec7f2433a9487478b48bd39f699cca3b00800d42
AMIT VERMA <amitverma.dce@gmail.com>
2023-05-18 10:07:23 +0530

    Slugify with pre save signals
    Using pre save signals for slugify the title text if it is None

--- config/articles/models.py
+++ config/articles/models.py
@@ -2,20 +2,34 @@ from typing import Iterable, Optional
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.db.models.signals import post_save, pre_save
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=50, blank=True, null=True)
     content = models.TextField()    
-    # auto_now means whenever model is saved current time will be set
-    # auto_now_add means whenever model is added this will be set
     timestamp = models.DateTimeField(auto_now_add=True)
-    # here timestamp make sense with added
     updated = models.DateTimeField(auto_now=True)
     # publish = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
     publish = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        # if self.slug is None:
+        #     self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    print("pre save")
+    print(sender, instance)
+    if (instance.slug == None):
+        instance.slug = slugify(instance.title)
+    print(args, kwargs)
+
+pre_save.connect(article_pre_save, sender=Article)
+
+def article_post_save(*args, **kwargs):
+    print("post save")
+    print(args, kwargs)
+
+post_save.connect(article_post_save, sender=Article)
