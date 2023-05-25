e072b3149f1c918e330915a02ddbaf1482801fe2
AMIT VERMA <amitverma.dce@gmail.com>
2023-05-25 16:31:02 +0530

    Custom validation with pint package
    Validation on the unit field

--- config/recipes/validators.py
+++ config/recipes/validators.py
@@ -1,6 +1,18 @@
+import pint
+from pint.errors import UndefinedUnitError
 from django.core.exceptions import ValidationError
-valid_unit_measurements = ['pounds', 'lbs', 'oz', 'gram']
+
+
+# def valid_unit_of_measure(value):
+#     if value not in valid_unit_measurements:
+#         raise ValidationError(f"{value} is not a valid unit of measure")
+
 
 def valid_unit_of_measure(value):
-    if value not in valid_unit_measurements:
+    ureg = pint.UnitRegistry()
+    try:
+        single_unit = ureg[value]
+    except UndefinedUnitError as e:
         raise ValidationError(f"{value} is not a valid unit of measure")
+    except:
+        raise ValidationError(f"{value} is invalid. Unknown error.")
